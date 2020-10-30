ce5ae4de4f32f16a703eec85bf7b248493f1e459
atik666 <faisal.atik@gmail.com>
2020-10-30 16:04:50 +0800

    10/30

--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    var resultText;
+    resultScore <= 8
+        ? resultText = 'You are awesome and innocent!'
+        : resultScore <= 12
+            ? resultText = 'Pretty likable!'
+            : resultScore <= 16
+                ? resultText = 'You are ... strange?!'
+                : resultText = 'You are so bad';
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('You did it'));
+    return Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+      resultPhrase,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 36,
+      ),
+    ),
+            Center(
+              child: FlatButton(
+                child: Text('Restart Quiz!'),
+                textColor: Colors.blue,
+                onPressed: resetQuiz,
+              ),
+            )
+          ],
+        ));
   }
 }
